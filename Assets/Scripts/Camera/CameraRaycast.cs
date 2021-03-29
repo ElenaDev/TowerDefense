277788545d77526ee5abb1544eff3923deec886c
Elenabf <eblanes7@gmail.com>
2021-03-29 21:42:10 +0200

    Clase 29 de Marzo. Player movement con raycast y quaternions

---
+++ Assets/Scripts/Camera/CameraRaycast.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraRaycast : MonoBehaviour
+{
+    public PlayerMovement playerMovement;//va a hacer referencia al componente PlayerMovement (el script)
+    public LayerMask groundLayer;
+
+    Ray ray;//rayo con punto de origen y dirección que uso para detectar gameobjects (tienen que tener colliders)
+    RaycastHit hit;//para guardarme la información de choque entre el rayo y el gameobject
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        //Si pulso el botón izquierdo del ratón
+        if(Input.GetMouseButtonDown(0))
+        {
+            //Camera.main hace referencia a la cámara de la escena que tenga la etiqueta de "MainCamera"
+            ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+            //esto nos devuelve un true si el raycast está chocando con un gameobject que tenga un collider y que esté en la capa
+            //suelo, si no se cumple esto me devuelve un false
+            if (Physics.Raycast(ray, out hit, 100, groundLayer))
+            {
+                //  Debug.Log("Hit Point " + hit.point);//hit.point es el punto de impacto entre rayo y suelo (Vector3)
+                playerMovement.posToGo = hit.point;
+                playerMovement.Turning(hit.point);
+            }
+            //dibuja el rayo en el panel escena
+            Debug.DrawRay(ray.origin, ray.direction * 10, Color.yellow);
+        }
+    }
+}
---
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public int speed;
+    public int turnSpeed;
+    public Vector3 posToGo;
+
+    Animator anim;
+    Quaternion rotation;
+
+    private void Awake()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+    void Start()
+    {
+        posToGo = transform.position;
+    }
+
+    void Update()
+    {
+        if(transform.position == posToGo)
+        {
+            anim.SetBool("IsRunning", false);
+        }
+        else
+        {
+            anim.SetBool("IsRunning", true);
+        }
+        transform.position = Vector3.MoveTowards(transform.position, posToGo, speed * Time.deltaTime);
+        transform.rotation = Quaternion.Lerp(transform.rotation, rotation, turnSpeed * Time.deltaTime);
+    }
+    public void Turning(Vector3 target)
+    {
+        //Vector dirección: vector que hay entre la posición a la que quiero mirar y mi posición
+        Vector3 direction = target - transform.position;
+        //creo una rotación, pásandole el vector3 que me da la dirección. Lo que hace es alinear el forward del personaje
+        //(es decir, hacia donde está mirando) con el vector que yo le paso (direction)
+        rotation = Quaternion.LookRotation(direction);
+        //con Lerp hacemos rotación gradual de un valor a otro    
+    }
+}
